b4c11ae3ffce7520d0c4cbd3d603f7b7e4c29046
Helios <35069323+wlhcode@users.noreply.github.com>
2018-11-12 23:51:20 +0800

    Update typing.cpp

--- typing.cpp
+++ typing.cpp
@@ -14,7 +14,7 @@ int main(){
 	srand(time(NULL));
 	s.clear();
 	ifstream in;
-	in.open("dihydrogen_monoxide.txt");
+	in.open("/*file name goes here*/");
 	while(getline(in,s)){
 		for(int i=0;i<s.length();i++){
 			cout<<s.at(i);
